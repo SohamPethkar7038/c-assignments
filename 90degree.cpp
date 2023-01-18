f1e0c9b3661aec2174f91fccc0c969a084ab8057
SohamPethkar7038 <122958042+SohamPethkar7038@users.noreply.github.com>
2023-01-18 12:25:48 +0530

    first assignment file
    i am commiting the first file

---
+++ 90degree.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int mat[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(mat[i][j],mat[j][i]);
+    }
+}
+//swap diagnols//
+ for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            swap(mat[i][j],mat[i][n-j-1]);
+    }
+}
+cout<<endl;
+//printing the matrix//
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        cout<<mat[i][j]<<" ";
+    }
+    cout<<endl;
+}
+return 0;
+}
---
+++ maxvalue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int mat[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat[i][j];
+        }
+    }
+    int val=INT_MIN;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(mat[i][j]>val){
+                val=mat[i][j];
+            }
+        }
+    }
+    cout<<"max value is "<<val;
+    return 0;
+}
